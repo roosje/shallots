b181e9749eaf6b865365bd49fdf8cd4b4ad2743e
gSchool Student <gSchool@Datas-Mac-mini-20.local>
2015-03-02 22:19:54 -0800

    fixed wrong table

--- setup/setup_postgres.py
+++ setup/setup_postgres.py
@@ -18,44 +18,34 @@ def create_database(dbname):
     except psycopg2.Error as e:
         print e
 
+def drop_table(tablename):
+    cur.execute("DROP TABLE IF EXISTS " + tablename)
+    con.comit()
 
 def create_tables():
     #create table sites    
-    cur.execute("select exists(select * from information_schema.tables where\
-                 table_name=%s)", ('sites',))
+    cur.execute("select exists(select * from information_schema.tables where table_name=%s)", ('sites',))
     if not cur.fetchone()[0]:
-        cur.execute("CREATE TABLE sites (site_id serial PRIMARY KEY, \
-                     mongo_id integer);")
-        
+        cur.execute("CREATE TABLE sites (site_id serial PRIMARY KEY, mongo_id integer);")
     #create table features 
     #!!!!!----------need to change to 1 column per extracted country----!!!!
-    cur.execute("select exists(select * from information_schema.tables where \
-                 table_name=%s)", ('features',))
+    cur.execute("select exists(select * from information_schema.tables where table_name=%s)", ('features',))
     if not cur.fetchone()[0]:
-        cur.execute("CREATE TABLE features (site_id integer, \
-                     cluster_id integer, countries varchar);")
-
+        cur.execute("CREATE TABLE features (site_id integer, cluster_id integer, countries varchar);")
     #create table clusters    
-    cur.execute("select exists(select * from information_schema.tables where \
-                 table_name=%s)", ('clusters',))
+    cur.execute("select exists(select * from information_schema.tables where table_name=%s)", ('clusters',))
     if not cur.fetchone()[0]:
-        cur.execute("CREATE TABLE clusters (site_id integer, \
-                     cluster_id integer, countries varchar);")
-        
+        cur.execute("CREATE TABLE clusters (cluster_id integer, cluster_name varchar, \
+                    legal boolean, description varchar);")
     #create table relations    
-    cur.execute("select exists(select * from information_schema.tables where \
-                 table_name=%s)", ('relations',))
+    cur.execute("select exists(select * from information_schema.tables where table_name=%s)", ('relations',))
     if not cur.fetchone()[0]:
-        cur.execute("CREATE TABLE relations (site_id_from integer, \
-                     site_id_to integer);")
-        
+        cur.execute("CREATE TABLE relations (site_id_from integer, site_id_to integer);")
     #create table clusterwordvecs    
-    cur.execute("select exists(select * from information_schema.tables where \
-                 table_name=%s)", ('clusterwordvecs',))
+    cur.execute("select exists(select * from information_schema.tables where table_name=%s)", ('clusterwordvecs',))
     if not cur.fetchone()[0]:
         cur.execute("CREATE TABLE clusterwordvecs (cluster_id integer, \
                     word_1 varchar, word_2 varchar, simscore real);")
-    
     con.commit()
     
 
@@ -64,8 +54,8 @@ if __name__ == '__main__':
     con = connect(database='shallots', user ='postgres', password='****', host='localhost')
     con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = con.cursor()
-    #drop everything
-    #
+    #drop tables
+    #drop_table('clusters')
     #create tables
     create_tables()
     #close connections
