04ab552ac5cff496a5e543c4b383890e74669308
gSchool Student <gSchool@Datas-Mac-mini-20.local>
2015-03-05 23:22:21 -0800

    in between

--- shallots/extractcountries.py
+++ shallots/extractcountries.py
@@ -3,6 +3,7 @@ import csv
 import pycountry
 import sqlite3
 import pandas as pd
+import pandas.io.sql as psql
 from collections import Counter #might need that later
 from fuzzywuzzy import fuzz
 import geograpy2
@@ -24,7 +25,7 @@ from sqlalchemy import create_engine
 #store pandas dataframe in sql
 
 def correct_country_mispelling(s):
-        with open("data/ISO3166ErrorDictionary.csv", "rb") as info:
+        with open("data/ISO3166ErrorDictionary2.csv", "rb") as info:
             reader = csv.reader(info)
             for row in reader:
                 if fuzz.ratio(s, row[0].decode('utf-8')) > 75:
@@ -42,16 +43,28 @@ def is_a_country2(s):
 
 def get_countries(s):
     places = geograpy2.get_place_context(text=s)
-    return set([correct_country_mispelling(place) for place \
-                        in set(places.names) if is_a_country2(place)])
+    return set([correct_country_mispelling(place) for place in set(places.names)\
+        if is_a_country2(correct_country_mispelling(place))])
 
-def run(client, dbname, ):
-    data = pd.DataFrame()
-    db = client.scrapy
+def run(db, con, cur):
+    print "extracting countries from text and storing"
+    data = psql.read_sql("SELECT domain, text from features limit 10;", con)
+    for index, row in data.iterrows():
+        print index
+        countries = get_countries(row['text'].decode('utf-8'))
+        for country in countries:
+            print country
+            data.loc[index, country]=True
+            #data[data['domain']==row['domain']][country]=True
 
-    
+    data = data.fillna(False)
+    print data.head(10)
+    #data.to_sql('features', con, if_exists='replace')
+
+'''
     engine = create_engine('postgresql://bla:bla@localhost:1234/mydatabase')
-    data['mongo_id'] = pd.read_sql_table('table_name', engine, index_col='mongo_id')
+    data['mongo_id'] = pd.read_sql_table('features', engine, index_col='mongo_id')
+    
     for id in data['mongo_id']:
         text = db.onions.find({'_id': id})['words']
         countries = get_countries(text)
@@ -59,7 +72,7 @@ def run(client, dbname, ):
             data[data['mongo_id']==id][country]=True
     
     #need to check if override or new table is best
-    data.to_sql('table_name', engine)
+    data.to_sql('table_name', engine)'''
 
 
 
--- shallots/shallots.py
+++ shallots/shallots.py
@@ -1,31 +1,41 @@
 import add_languages
 import setup_postgres
+import extractcountries
 
 import pymongo
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 from psycopg2 import connect
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+import sys
+import os
+import re
+
 class shallots(object):
     def __init__(self):
-        self.client = MongoClient('localhost', 27017)
-        self.dbname= 'shallots'
-        self.db = self.client.scrapy.onions
-        self.con = connect(database='shallots', user ='postgres', password='****', host='localhost')
+        password = os.environ['roosje_pass']
+        server = os.environ['aws_server']
+        self.client = MongoClient(server, 27017)
+        self.mongo_db = self.client.scrapy.onions
+        self.sql_dbname= 'shallots'
+        self.con = connect(database=self.sql_dbname, user ='postgres', \
+                           password=password, host=server)
         self.con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         self.cur = self.con.cursor()
         self.clusters = {}
-        self.comains =[]
+        self.domains = set()
 
     def add_languages_mongo(self, stepsize, start):
-        add_languages.run(self.db, stepsize, start)
+        add_languages.run(self.mongo_db, stepsize, start)
 
     def make_sql_database(self):
-        #create database ?
-        #create tables
-        tables = #@@@
+        #get current tables
+        self.cur.execute("SELECT relname FROM pg_class WHERE relkind='r' AND \
+                          relname !~ '^(pg_|sql_)';")
+        tables = [i[0] for i in self.cur.fetchall()]
         #drop all tables
         for table in tables:
             setup_postgres.drop_table(self.con, self.cur, table)
@@ -36,34 +46,62 @@ class shallots(object):
         #select only english
         #get mongo_id
         #store in sql
-        for i, d in self.db.find({"language": "en"})['_id', 'domain']
-            self.domains.append(d)
+        print "filling mongo ref table in sql"
+        for res in self.mongo_db.find({"language": "en"},{"_id":1, "domain":1}):
+            domainparts = res['domain'].split(".")
+            domain = domainparts[-2] + "." + domainparts[-1]
+            self.domains.add(domain)
             #insert into sites fields: mongo_id and domain, site_id is serial
-            self.cur.execute("INSERT INTO sites VALUES (%s, %s);", (i, d))
+            self.cur.execute('''INSERT INTO sites (mongo_id, domain) VALUES \
+                            ('%s', '%s');''' %(str(res['_id']), domain))
         self.con.commit()
         
     def fill_sitesite_relations(self):
+        print "filling dom-dom relations in sql"
+        self.cur.execute('SELECT DISTINCT(domain) FROM sites;')
+        self.domains = set([i[0] for i in self.cur.fetchall()])
+        pattern = re.compile('([2-7a-z]+\.onion)')
         for d in self.domains:
+            ref_domains = set()
             #get html from mongo for that domain
-            html = self.db.find({"domain": d}['HTML'])
+            htmls = self.mongo_db.find({"domain": d},{'html':1})
             #find domains in htmls
-            #store d, domains in relations
-        self.con.commit()    
+            for page in htmls:
+                results = pattern.findall(page['html'])
+                for res in results:
+                    ref_domains.add(res)
+            #discard d itself        
+            ref_domains.discard(d)
+            #discard every domain that is not in the database
+            ref_domains.intersection_update(self.domains)
+            for ref_dom in ref_domains:
+                #store d, domains in relations
+                self.cur.execute('''INSERT INTO relations VALUES \
+                                ('%s', '%s');''' %(d, ref_dom))
+            self.con.commit()    
+
+    def clean_text_store(self):
+        print "concat text per domain, clean and store"
+        self.cur.execute("SELECT DISTINCT(domain) FROM sites;")
+        self.domains = set([i[0] for i in self.cur.fetchall()])
+        for d in self.domains:
+            #search text and concat
+            text = " ".join(res['words'] for res in \
+                   self.mongo_db.find({"domain":d},{'words':1}))
+            #cleaning punctuation and non alpha content
+            '''
+            rx = re.compile('\W+')
+            text = rx.sub(" ", text).strip()
+            text = " ".join([word.lower() for word in text.split() if \
+                       word.isalpha()])'''
+            text = text.replace("'"," ")
+            #store in postgres
+            self.cur.execute('''INSERT INTO features (domain, text) VALUES('%s', '%s');'''\
+                             %(d, text))
+        self.con.commit()
 
     def fill_countries(self):
-        extractcountries.run(self.client)
-
-    def clean_text_store():
-        #group text per domain get from mongo
-        self.cur.execute("SELECT DISTINCT(domain) FROM sites;")
-        domains = self.cur.fetchall()
-        for d in domains:
-            text = " ".join(db.find({"domain":d})['words'].values)
-            #remove ascii
-            text = text.lower()
-            #store in postgres
-            self.cur.execute("INSERT INTO features VALUES(%s, %s);", (d, text))
-        self.con.commit()
+        extractcountries.run(self.mongo_db, self.con, self.cur)
 
     def find_clusters_descr_and_store(self, n_topics):
         self.clusters = topic_model.model(data, n_topics)
@@ -72,29 +110,29 @@ class shallots(object):
             self.cur.execute("INSERT INTO clusters VALUES(%d, %s);",(k, descr))
 
     def similar_extract(self):
-        conceptextractor.extract_and store(self.con, self.cur, self.clusters)
+        conceptextractor.extract_and_store(self.con, self.cur, self.clusters)
 
 if __name__ == '__main__':
-    #shal = shallots()
-    #add "language" field to mongodb
-    self.add_languages_mongo(stepsize =100, start=0)
-    break
-    #drop, make database & tables postgres
-    self.make_sql_database()
-    #fill tables with referrals to mongo ID (filter on english language)
-    self.fill_mongoref_sql_database()
-    #extract urls (domains) and store in table
-    self.fill_sitesite_relations()
-    #clean and concat text and store in sql
-    self.clean_text_store()
-    #extract countries and store in table
-    self.fill_countries()
-    #store clusters and their description
-    self.find_clusters_descr_and_store(n_topics = 10)
+    shal = shallots()
+    '''add "language" field to mongodb'''
+    #shal.add_languages_mongo(stepsize =1000, start=0)
+    '''drop, make database & tables postgres'''
+    #shal.make_sql_database()
+    '''fill tables with referrals to mongo ID (filter on english language)'''
+    #shal.fill_mongoref_sql_database()
+    '''extract urls (domains) and store in table'''
+    #shal.fill_sitesite_relations()
+    '''clean and concat text and store in sql'''
+    #shal.clean_text_store()
+    '''extract countries and store in table'''
+    shal.fill_countries()
+    sys.exit()
+    '''store clusters and their description'''
+    shal.find_clusters_descr_and_store(n_topics = 10)
     '''handwork annotating legal/illegal'''
-    #within clusters, do similar concept extraction and store in table
-    self.similar_extract()
-    #visualize (tell flask to prepare everything)
-    self.cur.close()
-    self.con.close()
-    self.client.close()
+    '''within clusters, do similar concept extraction and store in table'''
+    shal.similar_extract()
+    '''visualize (tell flask to prepare everything)'''
+    shal.cur.close()
+    shal.con.close()
+    shal.client.close()
